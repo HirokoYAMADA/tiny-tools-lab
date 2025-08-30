c07def00b70f05dde1fd5f680a15cc7443a5574a
Hiroko.YAMADA <45808217+HirokoYAMADA@users.noreply.github.com>
2025-08-30 20:06:10 +0900

    ホームページのタイトルをスタイル付きのh1要素でラップし、中央揃えとパディングを追加しました。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <div>
       {/* タイトル */}
-      <h1>Welcome to Tiny Tools Lab</h1>
+      <div className="">
+        <h1 className="text-2xl font-bold text-center py-10">Welcome to Tiny Tools Lab</h1>
+      </div>
+
       {/* ツール一覧 */}
       <div className="flex flex-wrap gap-4 justify-center ">
         <MainMenuCard href="/as_far_as_possible" title="遠くに行きたい" />
