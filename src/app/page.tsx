2592b32f2abff3107491d5ad66b7956a21c4c9d0
Hiroko.YAMADA <45808217+HirokoYAMADA@users.noreply.github.com>
2025-08-31 16:16:21 +0900

    ホームページのMainMenuCardコンポーネントにアイコンを追加し、レイアウトを調整しました。また、ConstructionIconとMapRouteIconの新しいコンポーネントを作成し、グローバルCSSにTailwind CSSアイコンプラグインを追加しました。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
+import { MapRouteIcon } from "@/components/atoms/MapRouteIcon";
+import { ConstructionIcon } from "@/components/atoms/ConstructionIcon";
 import { MainMenuCard } from "@/components/molecules/MainMenuCard";
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-full">
       {/* タイトル */}
       <div className="">
         <h1 className="text-2xl font-bold text-center py-10">Welcome to Tiny Tools Lab</h1>
       </div>
 
       {/* ツール一覧 */}
-      <div className="flex flex-wrap gap-4 justify-center ">
-        <MainMenuCard href="/as_far_as_possible" title="遠くに行きたい" />
-        <MainMenuCard title="工事中" />
+      <div className="flex flex-wrap gap-4 justify-center px-4">
+        <MainMenuCard href="/as_far_as_possible" title="遠くに行きたい" ><MapRouteIcon className="w-14 h-14" /></MainMenuCard>
+        <MainMenuCard title="工事中" ><ConstructionIcon className="w-14 h-14" /></MainMenuCard>
+        <MainMenuCard title="工事中" ><ConstructionIcon className="w-14 h-14" /></MainMenuCard>
       </div>
     </div>
   );
--- src/components/molecules/MainMenuCard.tsx
+++ src/components/molecules/MainMenuCard.tsx
@@ -1,17 +1,21 @@
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 
 type MainMenuCardProps = {
     href?: string;
     title: string;
+    children: React.ReactNode;
 }
 
-export function MainMenuCard({ href, title }: MainMenuCardProps) {
+export function MainMenuCard({ href, title, children }: MainMenuCardProps) {
     return (
         // hrefがあればCardをリンクにし、なければCardをそのまま表示 
         <Card className="aspect-square w-40" {...(href && { href: href })}>
             <CardHeader>
                 <CardTitle className="text-center font-medium text-sm">{title}</CardTitle>
             </CardHeader>
+            <CardContent className="flex justify-center items-center">
+                {children}
+            </CardContent>
         </Card >
     );
 }
